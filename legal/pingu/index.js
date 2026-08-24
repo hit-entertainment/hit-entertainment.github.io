c8b39b66845dbb70d0c5fdfeceedf68fe0e310bd
hit-entertainment <noahmitchell0607@gmail.com>
2026-08-24 15:55:36 -0400

    Add legal information and links to index.js

---
+++ legal/pingu/index.js
@@ -0,0 +1,7 @@
+document.write("<CENTER>");
+document.write("<FONT SIZE='1' FACE='Arial, sans serif, helvetica'>");
+document.write("<br>© 2009 The Pygos Group. All Rights Reserved.<br>");
+document.write("<a href='https://hitentertainment.github.io/privacy.html' target='_blank'>Privacy Statement</a><br>");
+document.write("<a href='https://hitentertainment.github.io/terms.html' target='_blank'>Terms and Conditions</a><br>");
+document.write("</FONT>");
+document.write("</CENTER>");
